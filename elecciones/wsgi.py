cbd1f6b78b7e4051d9469617938b54c6e5227ddf
inkarri <miguel.amaya99@gmail.com>
2014-09-29 00:53:25 -0500

    corrigiendo utf8

--- elecciones/wsgi.py
+++ elecciones/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+
+import sys
+reload(sys)     
+sys.setdefaultencoding("utf-8")
