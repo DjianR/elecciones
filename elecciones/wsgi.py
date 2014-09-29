588d5d9d3076b004387b70ba3754b7f469d650d9
inkarri <miguel.amaya99@gmail.com>
2014-09-28 23:11:58 -0500

    commit inicial

--- elecciones/wsgi.py
+++ elecciones/wsgi.py
@@ -11,8 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elecciones.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
